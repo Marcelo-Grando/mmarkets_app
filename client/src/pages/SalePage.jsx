d2069a84e57e84b8423a1be8bb5d19ce119a32d4
Marcelo-Grando <marcelogrando017@gmail.com>
2024-01-04 06:41:19 -0300

    Adding user state in UserHomePage

---
+++ client/src/pages/SalePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SalePage() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
--- client/src/pages/UserHomePage.jsx
+++ client/src/pages/UserHomePage.jsx
@@ -1,16 +1,27 @@
 import { useLocation } from "react-router-dom"
+import { useState } from "react"
+import { useEffect } from "react"
 
 import SellerHomePage from "./SellerHomePage"
 import MainHomePage from "./MainHomePage"
 import AdminHomePage from "./AdminHomePage"
 
+
 export default function UserHomePage() {
+  const [userData, setUserData] = useState()
+
 
     const {state} = useLocation()
+
+    useEffect(() => {
+      setUserData(state.userData)
+    }, [userData])
+
+    console.log(userData)
 
     const {name, lastname, email, dni, roles, user_id, market_id} = state.userData
 
     if(roles === "seller") {
-      return <SellerHomePage userData={state.userData}/>
+      return <SellerHomePage userData={userData}/>
     }
 }
